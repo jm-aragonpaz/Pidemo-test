bdc04dedeea29a54495a6f11b64426356f7fb9fb
Juan Manuel Aragon Paz <jm.aragonpaz@gmail.com>
2023-10-12 12:58:51 -0300

    feat: add materialUI

--- frontend/src/Shop/components/Mementor.tsx
+++ frontend/src/Shop/components/Mementor.tsx
@@ -1,15 +1,27 @@
+import {
+  Box,
+  Button,
+  CircularProgress,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import React, { useState } from "react";
 
 export default function Mementor() {
   const [apiResponse, setApiResponse] = useState<string | null>(null);
   const [textInput, setTextInput] = useState("");
   const [meme, setMeme] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTextInput(event.target.value);
   };
 
   const handleApiRequest = async () => {
+    setLoading(true);
     try {
       const apiUrl = "https://fastapi-production-2a5c.up.railway.app/get-meme/";
       const requestData = { texto: textInput, meme: meme };
@@ -32,25 +44,74 @@ export default function Mementor() {
     } catch (error) {
       console.error("Error:", error);
     }
+    setLoading(false);
   };
 
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h1>Mementor</h1>
-      <input
-        type="text"
-        placeholder="Ingrese el texto"
-        value={textInput}
-        onChange={handleInputChange}
-      />
-      <label>Selecciona un meme:</label>
-      <select value={meme} onChange={(e) => setMeme(e.target.value)}>
-        <option value="">Seleccionar</option>
-        <option value="disaster">Disaster</option>
-        <option value="distracted">Distracted</option>
-      </select>
-      <button onClick={handleApiRequest}>Get meme!</button>
-      {apiResponse && <img src={apiResponse} alt="Imagen de la API" />}
+      <p>
+        {/* <textarea
+          text="text"
+          placeholder="Input text"
+          value={textInput}
+          onChange={handleInputChange}
+        /> */}
+        <TextField
+          color="secondary"
+          label="Input text"
+          variant="outlined"
+          multiline
+          rows={4}
+          placeholder="Input text"
+          onChange={handleInputChange}
+          value={textInput}
+        />
+      </p>
+
+      {/* <label>Select a meme:</label> */}
+      <p>
+        <Box sx={{ width: 200, margin: "0 auto" }}>
+          <FormControl fullWidth>
+            <InputLabel color="secondary" id="select-label">
+              Select meme template
+            </InputLabel>
+            <Select
+              color="secondary"
+              labelId="select-label"
+              value={meme}
+              label="Select meme template"
+              onChange={(e) => setMeme(e.target.value)}
+              placeholder="Select a meme template"
+            >
+              <MenuItem value="disaster">Disaster girl</MenuItem>
+              <MenuItem value="distracted">Distracted boyfriend</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        {/* <select value={meme} onChange={(e) => setMeme(e.target.value)}>
+          <option value="" disabled>
+            Select a meme template
+          </option>
+          <option value="disaster">Disaster Girl</option>
+          <option value="distracted">Distracted Boyfriend</option>
+        </select> */}
+      </p>
+      {/* <button onClick={handleApiRequest}>Get meme!</button> */}
+      <Button
+        color="secondary"
+        onClick={handleApiRequest}
+        disabled={loading}
+        variant="outlined"
+      >
+        Get meme
+      </Button>
+      <p>
+        {loading && <CircularProgress color="secondary" />}
+        {apiResponse && !loading && (
+          <img width="800px" src={apiResponse} alt="Imagen de la API" />
+        )}
+      </p>
     </div>
   );
 }
